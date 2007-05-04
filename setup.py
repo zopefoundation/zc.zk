69d89adc2aa4f10b17abcac1b20f8c099ca56f5b
Jim Fulton <jim@zope.com>
2007-05-04 09:12:23 +0000

    Set up initial skel.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+entry_points = """
+"""
+
+setup(
+    name = ''
+    version = '0.1',
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = '',
+    license = 'ZPL 2.1',
+    
+    packages = find_packages('src'),
+    namespace_packages = ['zc'],
+    package_dir = {'': 'src'},
+    install_requires = 'setuptools',
+    zip_safe = False,
+    entry_points=entry_points,
+    )
