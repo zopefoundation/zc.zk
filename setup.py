37c5a7e228c95962568575a4d54b1d48ef7f730e
Jim Fulton <jim@zope.com>
2008-01-23 01:08:09 +0000

    spruced up

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import os
 from setuptools import setup, find_packages
 
 entry_points = """
 """
+
+def read(rname):
+    return open(os.path.join(os.path.dirname(__file__), *rname.split('/')
+                             )).read()
+
+long_description = (
+        read('src/zc/?/README.txt')
+        + '\n' +
+        'Download\n'
+        '--------\n'
+        )
+
+open('doc.txt', 'w').write(long_description)
 
 setup(
     name = '',
@@ -9,6 +36,7 @@ setup(
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = '',
+    long_description=long_description,
     license = 'ZPL 2.1',
     
     packages = find_packages('src'),
