bd5ea5de30465b95f5790538f470cc3987c27224
Jim Fulton <jim@zope.com>
2014-01-05 15:07:36 -0500

    release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = 'zc.zk', '0'
+name, version = 'zc.zk', '2.0.0a1'
 
 install_requires = ['setuptools', 'zc.thread', 'kazoo']
 extras_require = dict(
